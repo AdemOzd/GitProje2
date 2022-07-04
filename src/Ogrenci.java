f1ee802d550ba0127f0ff3196d1460f12fa8942c
adem7 <adem7146@hotmail.com>
2022-07-04 21:54:57 +0300

    dördüncü deneme

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -2,6 +2,7 @@ public class Ogrenci {
     public static void main(String[] args) {
         System.out.println("merhaba dünya 2");
         System.out.println("merhaba dünya 3");
+        System.out.println("dördüncü deneme");
 
     }
 }
